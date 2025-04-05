3ec2a64ec9fd58721fdce9d180964af588716c68
The <153374892+thedogthe@users.noreply.github.com>
2025-04-05 21:45:26 +0500

    Test commit

--- src/main/java/com/the/dailytasks/exception/TaskNotFoundException.java
+++ src/main/java/com/the/dailytasks/exception/TaskNotFoundException.java
@@ -1,5 +1,6 @@
 package com.the.dailytasks.exception;
 
+// Класс для обработки исключения TaskNotFound
 public class TaskNotFoundException extends RuntimeException {
     public TaskNotFoundException(String message) {
         super(message);
